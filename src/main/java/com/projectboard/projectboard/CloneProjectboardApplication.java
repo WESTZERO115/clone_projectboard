a21ecca42f8ecc8c300b79fbed84c4f4e3055f68
WESTZERO115 <psy7361c@gmail.com>
2023-11-07 00:52:54 +0900

    Initial commit

---
+++ src/main/java/com/projectboard/projectboard/CloneProjectboardApplication.java
@@ -0,0 +1,13 @@
+package com.projectboard.projectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloneProjectboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloneProjectboardApplication.class, args);
+    }
+
+}
