1338c06b10fe40d3b5b3690f26318913dad1e1f7
WESTZERO115 <psy7361c@gmail.com>
2023-12-15 18:29:29 +0900

    #16-게시글 관련 뷰 테스트 작성

---
+++ src/main/java/com/projectboard/projectboard/controller/ArticleController.java
@@ -0,0 +1,17 @@
+package com.projectboard.projectboard.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+/*
+*  /articles
+*  /articles/{articles-id}
+*  /articles/search
+*  /articles/search-hashtag
+* */
+
+@RequestMapping("/articles")
+@Controller
+public class ArticleController {
+}
