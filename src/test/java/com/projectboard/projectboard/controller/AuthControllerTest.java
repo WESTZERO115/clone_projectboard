ac9047e15e9af557cbf6e5e1733047df3614d312
WESTZERO115 <psy7361c@gmail.com>
2024-01-09 01:49:25 +0900

    #18 - 로그인 페이지 만들기 - 시큐리티 설정을 모든 요청에 인증이 열리도록 수정

---
+++ src/test/java/com/projectboard/projectboard/controller/AuthControllerTest.java
@@ -0,0 +1,34 @@
+package com.projectboard.projectboard.controller;
+
+import com.projectboard.projectboard.config.SecurityConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+@Import(SecurityConfig.class)
+@DisplayName("View 컨트롤러 - 인증")
+@WebMvcTest
+public class AuthControllerTest {
+
+    private final MockMvc mvc;
+    public AuthControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view] [GET] 로그인 페이지 - 정상 호출")
+    @Test
+    public void givenNothing_whenTryingToLogIn_thenReturnLogInView() throws Exception {
+        //Given
+        //When & Then
+        mvc.perform(MockMvcRequestBuilders.get("/login"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML));
+    }
+}
